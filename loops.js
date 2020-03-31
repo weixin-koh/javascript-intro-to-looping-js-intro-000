e682d1a03ed46abce2e7d89eb128f91fb231fbb7
Wei Xin Koh <e0201350@u.nus.edu>
2020-03-31 20:10:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -21,12 +21,13 @@ function whileLoop(number) {
 function doWhileLoop(number) {
   let i = 0;
 
+  function incrementVariable() {
+    i = i + 1;
+    return i;
+  }
+
+
   do {
     console.log("I run once regardless.");
   } while (incrementVariable() < number);
-}
-
-function incrementVariable() {
-  i = i + 1;
-  return i;
 }
